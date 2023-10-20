f933e714f8d6b819f8bac30d60239a8f715fa195
andooown <andooown.dev@gmail.com>
2023-10-21 01:05:44 +0900

    Update swift-syntax to 509.0.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-format.git", revision: "508.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", revision: "508.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", revision: "509.0.0"),
     ],
     targets: [
         .executableTarget(
