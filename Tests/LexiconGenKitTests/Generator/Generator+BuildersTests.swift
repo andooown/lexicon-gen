78dd9b4b1b382dc1382b2e950b036dca2532bd5f
andooown <andooown.dev@gmail.com>
2023-10-22 00:33:40 +0900

    Test namespaces

--- Tests/LexiconGenKitTests/Generator/Generator+BuildersTests.swift
+++ Tests/LexiconGenKitTests/Generator/Generator+BuildersTests.swift
@@ -7,8 +7,35 @@ final class GeneratorBuildersTests: XCTestCase {
     func testNamespaces() throws {
         XCTAssertNoDifference(
             try Generator.namespaces([
-            ]).description,
+                SwiftNamespaceDefinition(parent: "", name: "Foo"),
+            ]).formatted().description,
             """
+            public enum Foo {
+            }
+            """
+        )
+        XCTAssertNoDifference(
+            try Generator.namespaces([
+                SwiftNamespaceDefinition(parent: "", name: "Foo"),
+                SwiftNamespaceDefinition(parent: "Foo", name: "Bar"),
+                SwiftNamespaceDefinition(parent: "Foo.Bar", name: "Baz"),
+                SwiftNamespaceDefinition(parent: "Foo.Bar", name: "Qux"),
+            ]).formatted().description,
+            """
+            public enum Foo {
+            }
+            public extension Foo {
+                enum Bar {
+                }
+            }
+            public extension Foo.Bar {
+                enum Baz {
+                }
+            }
+            public extension Foo.Bar {
+                enum Qux {
+                }
+            }
             """
         )
     }
